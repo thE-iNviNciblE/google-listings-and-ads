85613d54e3a21717c0017da22558e4262172afb4
Jason Conroy <jason@findingsimple.com>
2021-01-12 11:45:07 +1030

    Example using options data store to retrieve and store a value

--- js/src/get-started-page/index.js
+++ js/src/get-started-page/index.js
@@ -1,17 +1,54 @@
 /**
+ * External dependencies
+ */
+import { compose } from '@wordpress/compose';
+import { withDispatch, withSelect } from '@wordpress/data';
+import { OPTIONS_STORE_NAME } from '@woocommerce/data';
+import { __experimentalInputControl as InputControl } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import './index.scss';
 import Faqs from './faqs';
 import GetStartedCard from './get-started-card';
 
-const GetStartedPage = () => {
+const GetStartedPage = ( { exampleOption, updateOptions } ) => {
+
+	const doUpdate = ( newValue ) => {
+
+		console.log( newValue );
+
+		updateOptions( {
+			gla_example_option: newValue,
+		} );
+	};
+
 	return (
 		<div className="woocommerce-marketing-google-get-started-page">
+			<InputControl
+				value={ exampleOption }
+				onChange={ ( newValue ) => doUpdate( newValue ) }
+			/>
 			<GetStartedCard />
 			<Faqs></Faqs>
 		</div>
 	);
 };
 
-export default GetStartedPage;
+// default export
+export default compose(
+	withSelect( ( select ) => {
+		const { getOption } = select( OPTIONS_STORE_NAME );
+		const exampleFromDB = getOption( 'gla_example_option' ) || '';
+		return {
+			exampleOption: exampleFromDB,
+		};
+	} ),
+	withDispatch( ( dispatch ) => {
+		const { updateOptions } = dispatch( OPTIONS_STORE_NAME );
+		return {
+			updateOptions,
+		};
+	} )
+)( GetStartedPage );
